6f30227358e12552f8f494f06dbbbb35e8c5f6b3
Daniel Valencia <danvalencia@gmail.com>
2012-05-28 13:55:10 -0700

    Rendering the Markdown output

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,13 @@
+require 'redcarpet'
+
 class Post < ActiveRecord::Base
   attr_accessible :body, :excerpt, :title
   validates_presence_of :title, :body
+  
+  def body
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML,
+            :autolink => true, :space_after_headers => true, :strikethrough => true, :superscript => true)    
+    markdown.render read_attribute(:body)
+  end
   
 end
