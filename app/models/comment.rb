864c3e8e2b82f687d5919756e4bde8befc0a3b97
Daniel Valencia <danvalencia@gmail.com>
2012-08-22 19:01:56 -0700

    parent_post is a required field for a comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,6 @@ class Comment < ActiveRecord::Base
   attr_accessible :body, :parent_comment_id, :parent_post_id
   belongs_to :parent_post, :foreign_key => 'parent_post_id', :class_name => 'Post'
   belongs_to :parent_comment, :foreign_key => 'parent_comment_id', :class_name => 'Comment'
+
+  validates_presence_of :parent_post, :body
 end
