d8ad50f0662b79c6ff0a2e58b146188cabcc2d21
Daniel Valencia <danvalencia@gmail.com>
2012-10-15 10:30:20 -0700

    Fixing validation, fixing broken tests

--- app/models/comment_author.rb
+++ app/models/comment_author.rb
@@ -3,9 +3,8 @@ class CommentAuthor < ActiveRecord::Base
 
   has_many :comments
 
-  validates :email, :presence => true, :allow_blank => false, :uniqueness => true, :length => {:maximum => 50}
-  validates :name, :allow_blank => true, :length => {:maximum => 300}
-  validates :website, :allow_blank => true, :length => {:maximum => 60}
+  validates :email, :presence => true, :allow_blank => false, :uniqueness => true
+  validates :name, :presence => true, :allow_blank => false
 
   accepts_nested_attributes_for :comments
 end
