3ece3de4913df937a41e3157314b0bb262217b8d
Daniel Valencia <danvalencia@gmail.com>
2012-10-15 16:29:57 -0700

    Removing unnecessary migration

--- db/migrate/20121002034059_rename_comment_author_user_to_name.rb
+++
@@ -1,9 +0,0 @@
-class RenameCommentAuthorUserToName < ActiveRecord::Migration
-  def up
-  	rename_column :comment_authors, :user, :name
-  end
-
-  def down
-  	rename_column :comment_authors, :name, :user
-  end
-end
