30d4c1baa04e8663e395a1be9b6887180e97339d
Daniel Valencia <danvalencia@gmail.com>
2012-10-15 16:24:37 -0700

    This should fix the issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Mysite::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( search.js global.css shCoreEclipse.css shThemeEclipse.css Markdown.Converter.js Markdown.Sanitizer.js Markdown.Editor.js shCore.js shBrushJava.js shBrushXml.js markdown_editor.css )   
+  config.assets.precompile += %w( search.js global.css shCoreEclipse.css shThemeEclipse.css Markdown.Converter.js Markdown.Sanitizer.js Markdown.Editor.js shCore.js shBrushJava.js shBrushXml.js markdown_editor.css markdown_editor_overrides.css)   
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
