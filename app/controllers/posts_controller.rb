9db7b51c76c57a67c15e131368a98290066f5869
Daniel Valencia <danvalencia@gmail.com>
2012-10-28 21:30:55 -0700

    Getting posts last one first

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_filter :authenticate_user!, :only => [:new, :create, :destroy, :update]
   
   def index
-    @posts = Post.all    
+    @posts = Post.order("created_at DESC")
   end
   
   def show
